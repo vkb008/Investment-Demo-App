04e86cc0ab800a355128404233e4d19b19c34dcb
Vikram Babu <vkb008@gmail.com>
2023-01-24 20:40:30 -0800

    Update index.js
    Display all transactions from sandbox brokerage account, not just 8 most recent.

--- node/index.js
+++ node/index.js
@@ -260,7 +260,9 @@ app.get('/api/transactions', function (request, response, next) {
 
       const compareTxnsByDateAscending = (a, b) => (a.date > b.date) - (a.date < b.date);
       // Return the 8 most recent transactions
-      const recently_added = [...added].sort(compareTxnsByDateAscending).slice(-8);
+      //const recently_added = [...added].sort(compareTxnsByDateAscending).slice(-8);
+      // Return the all transactions
+      const recently_added = [...added].sort(compareTxnsByDateAscending);
       response.json({latest_transactions: recently_added});
     })
     .catch(next);
